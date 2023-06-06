b2bd05314ce11654c4c4c00b703d9a23c2607653
firefly-ge <67929411@qq.com>
2023-06-07 01:35:16 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,16 +1,9 @@
 # run.sh
 # 切换到jar包目录下
-cd /root/GitHub_Action
-# 杀死之前的项目进程。这里的cut -c9-14是截取进程id，不一定都是-c9-14
-# 可以先运行ps -ef|grep java|grep GitHub_Action-0.0.1-SNAPSHOT.jar看一下进程id是第几位到第几位
-# ps -ef|grep java|grep demo-0.0.1-SNAPSHOT.jar|cut -c9-14|xargs kill -9
-# ps -ef | grep java | grep -v grep | awk '{print $2}' | xargs kill -9
-source /root/.bash_profile
+cd /root/GitHub_Action 
 # 停5秒
 sleep 5s
-# 使环境变量生效ddd
+# 使环境变量生效
 source /root/.bash_profile
 # 运行项目
-pkill -9 java
-
 nohup java -jar demo-0.0.1-SNAPSHOT.jar >/root/cicd.log 2>&1 &
