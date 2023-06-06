08ffbf9209097d2461e649bcab87713d7b16d7b9
13675143343@163.com <2615381>
2023-06-06 23:59:12 +0800

    4444

--- src/main/java/com/run/demo/IndexController.java
+++ src/main/java/com/run/demo/IndexController.java
@@ -4,7 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-@Controller("/aa")
+@Controller
+@RequestMapping("/aa")
 public class IndexController {
     @RequestMapping("/index")
     @ResponseBody
